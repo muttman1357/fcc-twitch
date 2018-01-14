9641e0e7e0a891cae093004d14b3e54993a077bf
Ron Mares <ron.mares@mktec.com>
2018-01-14 15:56:46 -0500

    twitch upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: []
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  public type = 'all';
+  public streams: any = [];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getStreams('all');
+  }
+
+  getStreams(type) {
+      this.http.get('https://api.twitch.tv/helix/streams/?type=' + type,
+        {headers: new HttpHeaders().set('Client-ID', 'j3ta4qnp53hv7t867vxyan7wwzkld6')})
+    .subscribe(data => {
+        // Read the result field from the JSON response.
+      this.setImgSize(data);
+      this.streams = data.data;
+        console.log(data);
+      });
+  }
+
+  setImgSize(data) {
+    data.data.forEach((obj) => {
+      obj.thumbnail_url = obj.thumbnail_url.replace(/{width}|{height}/g, 50);
+    });
+  }
+
+  switchType(e, type) {
+    e.preventDefault();
+    this.type = type;
+    this.getStreams(type);
+  }
 }
